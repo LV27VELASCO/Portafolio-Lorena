27349421daf67fea40a1a4e2ba132a6586416be4
Luis Velasco <luiselsw27@gmail.com>
2022-08-15 11:57:18 -0500

    seccion de colaboraciones lista

--- JavaScript.js
+++ JavaScript.js
@@ -3,6 +3,8 @@ const d= document
 const $menu=d.querySelector('.menu')
 const $navbar=d.querySelector('.navbar')
 const $iconMenu=d.querySelector('.menu i')
+const $cards=d.querySelectorAll('#container_card .info_card' )
+
 
 $menu.addEventListener('click',()=>{
     if($navbar.className==='navbar active'){
@@ -23,4 +25,25 @@ $(d).ready(function(){
         delay:13,
         time:1600
     })
+})
+
+
+function onClick(e){
+
+    // $cards.forEach(card => {
+    //     card.classList.remove('active')
+    // })
+
+   const evento= e.path[2]
+   
+   if (evento.className==='info_card') {
+    console.log(evento.classList.add('active'))
+   }else{ evento.classList.remove('active')}
+   }
+
+
+
+
+$cards.forEach(card =>{
+    card.addEventListener('click',onClick)
 })
