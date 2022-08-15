edb07db9147d082124fbfdd0684d179571dae35d
Luis Velasco <luiselsw27@gmail.com>
2022-08-15 14:14:48 -0500

    video deja de sonar al esconder la card

--- JavaScript.js
+++ JavaScript.js
@@ -3,7 +3,8 @@ const d= document
 const $menu=d.querySelector('.menu')
 const $navbar=d.querySelector('.navbar')
 const $iconMenu=d.querySelector('.menu i')
-const $cards=d.querySelectorAll('#container_card .info_card' )
+const $cards=d.querySelectorAll('#container_card .item' )
+const $nadena=d.getElementById('nadena')
 
 
 $menu.addEventListener('click',()=>{
@@ -29,19 +30,66 @@ $(d).ready(function(){
 
 
 function onClick(e){
+    
 
     // $cards.forEach(card => {
     //     card.classList.remove('active')
     // })
-
-   const evento= e.path[2]
    
-   if (evento.className==='info_card') {
-    console.log(evento.classList.add('active'))
-   }else{ evento.classList.remove('active')}
+
+   const evento= e.path[2].children[1]
+   
+
+   if(evento.className==='info_item nadena'){
+    evento.classList.add('active')
+    evento.innerHTML=`
+        <span><i class='bx bx-video-recording'></i>Jul 26, 2022</span>
+        <h3>MODELING IN THE SINGLE </h3>
+        <p> "NA DE NA"</p>
+        <iframe width="100%" height="250" src="https://www.youtube.com/embed/3fwNh5AVWJY" 
+        title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+        clipboard-write;encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+
+
+   else if(evento.className==='info_item sexy'){
+    evento.classList.add('active')
+    evento.innerHTML=`
+    <span><i class='bx bx-music'></i> May 28, 2022</span>
+    <h3>NEW SINGLE </h3>
+    <p>"SEXY MALOSO"</p>
+    <iframe width="100%" height="250" src="https://www.youtube.com/embed/AmeBTKrKM8U" 
+    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+    clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+    `
+}
+   else if(evento.className==='info_item stuck'){
+    evento.classList.add('active')
+    evento.innerHTML=`
+    <span><i class='bx bx-music'></i> May 19, 2020</span>
+    <h3>SINGLE (COVER) spanish</h3>
+    <p> "STUCK-WITH"</p>
+    <iframe width="100%" height="250" src="https://www.youtube.com/embed/HWJOxr5jv90" 
+    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+    clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `
+}
+   else if(evento.className==='info_item creeme'){
+    evento.classList.add('active')
+    evento.innerHTML=`
+    <span><i class='bx bx-music'></i> Dic 7, 2018</span>
+    <h3>SINGLE (COVER)</h3>
+    <p>"CRÉEME"</p>
+    <iframe width="100%" height="250" src="https://www.youtube.com/embed/FabzV5JTijQ?start=4" 
+    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+    clipboard-write;encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `
+}
+   else{
+    evento.classList.remove('active') 
+    evento.innerHTML=''
    }
-
-
+   }
 
 
 $cards.forEach(card =>{
